89a3469a8cb811754e1aab86ddb1a018b880f69e
AnaProgramando <anabe.valentim@gmail.com>
2021-08-25 16:52:59 -0300

    Criado Funcionario padrão

---
+++ Funcionarios/Funcionario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Heranca_e_interface.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public double GetBonificacao()
+        {
+            return Salario * 0.10;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharp_Heranca_e_interface.Funcionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,16 @@ namespace CSharp_Heranca_e_interface
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            Funcionario fulano = new Funcionario();
+            fulano.Nome = "Fulano";
+            fulano.CPF = "123.456.789-10";
+            fulano.Salario = 2000;
+
+            Console.WriteLine(fulano.Nome);
+            Console.WriteLine(fulano.GetBonificacao());                  
+
+            Console.ReadLine();
         }
     }
 }
